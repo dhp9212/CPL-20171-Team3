7904cf89675c83ab694dcb124abc9e16eda2c8af
seunghyeun <dltmdgus0426@naver.com>
2017-04-23 03:10:36 +0900

    raspberry-arduino serial basic
    win에서 실시한 arduino 시리얼 통신(java)

---
+++ Source code/Arduino/my_basic_source.ino
@@ -0,0 +1,25 @@
+int incomingByte = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0)
+  {
+    //read the incoming byte
+
+    Serial.write(" : ");
+    incomingByte = Serial.read();
+    //byte to chart
+    char myChar = char(incomingByte);
+    
+    //Serial.print(" recived : ");
+    //Serial.println(myChar);
+    Serial.write(incomingByte);
+    
+    
+  }
+}
