a463849c62cad404a93da7b94f3f6236c1421a2c
anstjq9 <anstjq9@naver.com>
2017-06-01 23:06:17 +0900

    WifiApp (+apk) 최종

---
+++ WifiApp/app/src/main/java/kr/soen/wifiapp/CameraFragment.java
@@ -0,0 +1,168 @@
+package kr.soen.wifiapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class CameraFragment extends Fragment implements View.OnClickListener{
+
+    WebView mWebView;
+    EditText alertAddress;
+
+    AlertDialog dialog;
+
+    boolean isFirst = true;
+    String address = null;
+
+    public CameraFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            isFirst = savedInstanceState.getBoolean("isFirst");
+            address = savedInstanceState.getString("address");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean("isFirst", isFirst);
+        savedInstanceState.putString("address", address);
+    }
+
+    @Override
+    @SuppressLint( "SetJavaScriptEnabled" )
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_camera, container, false);
+
+        ImageButton backBtn = (ImageButton)layout.findViewById(R.id.btn_back);
+        backBtn.setOnClickListener(this);
+
+        TextView titleText = (TextView)layout.findViewById(R.id.title_text) ;
+        titleText.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareEB.ttf"));
+
+        /////////////
+
+        mWebView = (WebView)layout.findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setPadding(0,0,0,0);
+        mWebView.getSettings().setBuiltInZoomControls(false);
+        mWebView.setInitialScale(160);
+
+        if (isFirst)
+        {
+            String str = "";
+            mWebView.loadUrl(str);
+        }
+        else
+        {
+            String str = "http://" + address + "/javascript_simple.html";
+            mWebView.loadUrl(str);
+        }
+
+        ////////////
+
+        LayoutInflater inflater1 = (LayoutInflater)getActivity().getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater1.inflate(R.layout.alert_layout, null);
+
+        TextView alertTitle = (TextView)view.findViewById(R.id.alert_title);
+        alertTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareL.ttf"));
+
+        TextView alertMsg = (TextView)view.findViewById(R.id.alert_msg);
+        alertMsg.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareR.ttf"));
+
+        alertAddress = (EditText) view.findViewById(R.id.address);
+        alertAddress.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareR.ttf"));
+        alertAddress.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    alertAddress.setHint("");
+                }
+                return false;
+            }
+        });
+        alertAddress.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        alertAddress.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE)
+                {
+                    address = alertAddress.getText().toString();
+                    String str = "http://" + address + "/javascript_simple.html";
+                    mWebView.loadUrl(str);
+                    isFirst = false;
+                    dialog.dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button alertBtn = (Button)view.findViewById(R.id.ok_btn);
+        alertBtn.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareR.ttf"));
+        alertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                address = alertAddress.getText().toString();
+                String str = "http://" + address + "/javascript_simple.html";
+                mWebView.loadUrl(str);
+                isFirst = false;
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+        ad.setView(view);
+
+        dialog = ad.create();
+
+        //////////////
+        TextView txt = (TextView)layout.findViewById(R.id.camera_text);
+        txt.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareL.ttf"));
+
+        return layout;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser)
+    {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser)
+        {
+            if(isFirst)
+            {
+                dialog.show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_back:
+                ((MainActivity) getActivity()).vp.setCurrentItem(1);
+                break;
+        }
+    }
+}
---
+++ WifiApp/app/src/main/java/kr/soen/wifiapp/TempHumFragment.java
@@ -0,0 +1,338 @@
+package kr.soen.wifiapp;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class TempHumFragment extends Fragment implements View.OnClickListener{
+    public static final String REQUEST_CURRENT_TEMP_HUM = "1";
+    public static final String REQUEST_ACCRUE_TEMP = "2";
+
+    public static final int TEMP_HUM = 1;
+    public static final int HOURLY_TEMP = 2;
+
+    float[] current_temp = new float[2];
+    float[] hum = new float[2];
+
+    float[] hot_accrue_tempTokens = new float[8];
+    float[] cool_accrue_tempTokens = new float[8];
+
+    TextView todayText;
+    TextView hotzoneTemp, coolzoneTemp, currentHum, avgHum;
+    LineChart accruetempChart;
+
+    boolean isFirst = true;
+
+    public TempHumFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_temp_hum, container, false);
+
+        ImageButton profileBtn = (ImageButton)layout.findViewById(R.id.btn_profile);
+        profileBtn.setOnClickListener(this);
+
+        TextView titleText = (TextView)layout.findViewById(R.id.title_text) ;
+        titleText.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareEB.ttf"));
+
+        TextView dummy = (TextView)layout.findViewById(R.id.dummy);
+        dummy.setOnClickListener(this);
+
+        ImageButton cameraBtn = (ImageButton)layout.findViewById(R.id.btn_camera);
+        cameraBtn.setOnClickListener(this);
+
+        ///////////////
+
+        todayText = (TextView)layout.findViewById(R.id.today) ;
+        todayText.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "CODE Light.otf"));
+        setToday();
+
+        ///////////////
+
+        TextView hotzoneTitle = (TextView)layout.findViewById(R.id.hotzone_title);
+        hotzoneTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        hotzoneTemp = (TextView)layout.findViewById(R.id.hotzone_text);
+        hotzoneTemp.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        TextView hotzoneUnit = (TextView)layout.findViewById(R.id.hotzone_unit);
+        hotzoneUnit.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+
+        TextView coolzoneTitle = (TextView)layout.findViewById(R.id.coolzone_title);
+        coolzoneTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        coolzoneTemp = (TextView)layout.findViewById(R.id.coolzone_text);
+        coolzoneTemp.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        TextView coolzoneUnit = (TextView)layout.findViewById(R.id.coolzone_unit);
+        coolzoneUnit.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+
+        TextView currenthumTitle = (TextView)layout.findViewById(R.id.currenthum_title);
+        currenthumTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        currentHum = (TextView)layout.findViewById(R.id.currenthum_text);
+        currentHum.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        TextView currenthumUnit = (TextView)layout.findViewById(R.id.currenthum_unit);
+        currenthumUnit.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+
+        TextView avghumTitle = (TextView)layout.findViewById(R.id.avghum_title);
+        avghumTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        avgHum = (TextView)layout.findViewById(R.id.avghum_text);
+        avgHum.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        TextView avghumUnit = (TextView)layout.findViewById(R.id.avghum_unit);
+        avghumUnit.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareB.ttf"));
+
+        ///////////////
+
+        TextView linechartTitle = (TextView)layout.findViewById(R.id.line_chart_tile);
+        linechartTitle.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareEB.ttf"));
+
+        accruetempChart = (LineChart) layout.findViewById(R.id.accrue_temp_chart);
+        accruetempChart.getAxisLeft().setEnabled(false);
+        accruetempChart.getAxisRight().setEnabled(false);
+        accruetempChart.getAxisLeft().setAxisMaximum(32);
+        accruetempChart.getAxisLeft().setAxisMinimum(15);
+        accruetempChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        accruetempChart.getXAxis().setTextSize(8);
+        accruetempChart.getXAxis().setTextColor(getResources().getColor(R.color.white));
+        accruetempChart.getXAxis().setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareR.ttf"));
+        accruetempChart.getDescription().setEnabled(false);
+        accruetempChart.setTouchEnabled(false);
+        accruetempChart.setDragEnabled(false);
+        accruetempChart.setScaleEnabled(false);
+        accruetempChart.getLegend().setEnabled(false);
+
+        return layout;
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(this.getClass().getSimpleName(), "onResume()");
+        super.onResume();
+
+        if(((MainActivity)getActivity()).id.equals("aa"))
+        {
+            float[] hotzoneHourly_m = { 28.3F, 27.7F, 29.5F, 27.0F, 29.1F, 25.3F, 25.0F, 27.1F};
+            float[] coolzoneHourly_m = { 23.1F, 18.7F, 20.4F, 20.7F, 22.1F, 17.9F, 18.6F, 17.0F, 17.5F};
+
+            setData(27.5F, 17.5F, 57.7F, 52.5F);
+            setGraphData(hotzoneHourly_m, coolzoneHourly_m);
+        }
+        else
+        {
+            ((MainActivity)getActivity()).doCommu(REQUEST_CURRENT_TEMP_HUM);
+            ((MainActivity)getActivity()).doCommu(REQUEST_ACCRUE_TEMP);
+
+            setValue(((MainActivity)getActivity()).currentTempHumData, TEMP_HUM);
+            setValue(((MainActivity)getActivity()).accrueTempData, HOURLY_TEMP);
+
+            setData(current_temp[0], current_temp[1], hum[0], hum[1]);
+            setGraphData(hot_accrue_tempTokens, cool_accrue_tempTokens);
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser)
+    {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser)
+        {
+            if(!isFirst)
+            {
+                if(!((MainActivity)getActivity()).id.equals("aa"))
+                {
+                    ((MainActivity)getActivity()).doCommu(REQUEST_CURRENT_TEMP_HUM);
+                    setData(current_temp[0], current_temp[1], hum[0], hum[1]);
+                }
+
+                accruetempChart.animateX(2000);
+            }
+            isFirst = false;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_profile:
+                ((MainActivity)getActivity()).vp.setCurrentItem(0);
+                break;
+            case R.id.dummy:
+                ((MainActivity)getActivity()).logMessege("내부환경이 적절치 않습니다.");
+                break;
+            case R.id.btn_camera:
+                ((MainActivity)getActivity()).vp.setCurrentItem(2);
+                break;
+        }
+    }
+
+    public void setToday()
+    {
+        long now = System.currentTimeMillis();
+        Date today = new Date(now);
+        SimpleDateFormat dateFormat = new  SimpleDateFormat("yyyy.MM.dd", java.util.Locale.getDefault());
+        String todayStr = dateFormat.format(today);
+
+        Calendar cal = Calendar.getInstance();
+        String dayStr = null;
+        switch (cal.get(Calendar.DAY_OF_WEEK))
+        {
+            case Calendar.SUNDAY:
+                dayStr = "SUN.";
+                break;
+            case Calendar.MONDAY:
+                dayStr = "MON.";
+                break;
+            case Calendar.TUESDAY:
+                dayStr = "TUE.";
+                break;
+            case Calendar.WEDNESDAY:
+                dayStr = "WED.";
+                break;
+            case Calendar.THURSDAY:
+                dayStr = "THU.";
+                break;
+            case Calendar.FRIDAY:
+                dayStr = "FRI.";
+                break;
+            case Calendar.SATURDAY:
+                dayStr = "SAT.";
+                break;
+        }
+
+        todayText.setText(todayStr + " " + dayStr);
+    }
+
+    public void setValue(String valueSet, int state)
+    {
+        StringTokenizer str = new StringTokenizer(valueSet, "/");
+
+        int countTokens = str.countTokens();
+
+        if(state == TEMP_HUM)
+        {
+            for (int i = 0; i < countTokens; i++)
+            {
+                if(i < 2)
+                {
+                    current_temp[i] = Float.parseFloat(str.nextToken());
+                }
+                else
+                {
+                    hum[i - 2] = Float.parseFloat(str.nextToken());
+                }
+            }
+        }
+        else if(state == HOURLY_TEMP)
+        {
+            for (int i = 0; i < countTokens; i++)
+            {
+                if (i < 8)
+                {
+                    hot_accrue_tempTokens[i] = Float.parseFloat(str.nextToken());
+                }
+                else
+                {
+                    cool_accrue_tempTokens[i - 8] = Float.parseFloat(str.nextToken());
+                }
+            }
+        }
+    }
+
+    public void setData(float hotzone, float coolzone, float currenthum, float avghum)
+    {
+        hotzoneTemp.setText(Float.toString(hotzone));
+        coolzoneTemp.setText(Float.toString(coolzone));
+        currentHum.setText(Float.toString(currenthum));
+        avgHum.setText(Float.toString(avghum));
+    }
+
+    public void setGraphData(float[] hotzone, float[] coolzone)
+    {
+        List<ILineDataSet> tempDataSet = new ArrayList<>();
+
+        ArrayList<Entry> hotzoneDataEntry = new ArrayList<>();
+        for (int i = 0; i < 8; i++)
+        {
+            hotzoneDataEntry.add(new Entry(i, hotzone[i]));
+        }
+        LineDataSet hotzoneDataSet = new LineDataSet(hotzoneDataEntry, "HotZone");
+        hotzoneDataSet.setColor(getResources().getColor(R.color.colorAccent));
+        hotzoneDataSet.setValueTextSize(10);
+        tempDataSet.add(hotzoneDataSet);
+
+        ArrayList<Entry> coolzoneDataEntry = new ArrayList<>();
+        for (int i = 0; i < 8; i++)
+        {
+            coolzoneDataEntry.add(new Entry(i, coolzone[i]));
+        }
+        LineDataSet coolzoneDataSet = new LineDataSet(coolzoneDataEntry, "CoolZone");
+        coolzoneDataSet.setColor(getResources().getColor(R.color.colorPrimary));
+        coolzoneDataSet.setValueTextSize(10);
+        tempDataSet.add(coolzoneDataSet);
+
+        LineData accruetempData = new LineData(tempDataSet);
+        accruetempData.setValueTextColor(getResources().getColor(R.color.white));
+        accruetempData.setValueTypeface(Typeface.createFromAsset(getActivity().getAssets(), "NanumSquareR.ttf"));
+        accruetempChart.setData(accruetempData);
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdfNow = new SimpleDateFormat("HH");
+        int formatHour = Integer.parseInt(sdfNow.format(date));
+
+        final String[] xAxis = new String[8];
+        for (int i = 0; i < 8; i++)
+        {
+            if(formatHour - ((7 - i)*3) < 0)
+            {
+                xAxis[i] = "어제 " + Integer.toString(24 + formatHour - ((7 - i)*3)) + "시";
+            }
+            else
+            {
+                xAxis[i] = "오늘 " + Integer.toString(formatHour - ((7 - i)*3)) + "시";
+            }
+        }
+
+        IAxisValueFormatter formatter = new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return xAxis[(int) value];
+            }
+        };
+
+        accruetempChart.getXAxis().setValueFormatter(formatter);
+        accruetempChart.getXAxis().removeAllLimitLines();
+
+        accruetempChart.animateX(2000);
+    }
+}
